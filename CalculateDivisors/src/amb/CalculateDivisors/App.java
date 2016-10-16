edc12fc431c595b49d8604583957157f413eb882
AlexanderB92 <a.m.brandstrup@gmail.com>
2016-10-15 23:39:37 -0400

    Implemented findPrimeFactors method, started app class for test run

--- CalculateDivisors/src/amb/CalculateDivisors/App.java
+++ CalculateDivisors/src/amb/CalculateDivisors/App.java
@@ -4,6 +4,9 @@ public class App {
 
 	public static void main(String[] args) {
 		
+		Divisor d = new Divisor(123);
+		
+		
 	}
 
 }
--- CalculateDivisors/src/amb/CalculateDivisors/Divisor.java
+++ CalculateDivisors/src/amb/CalculateDivisors/Divisor.java
@@ -3,36 +3,51 @@ package amb.CalculateDivisors;
 import java.util.ArrayList;
 
 public class Divisor {
-	
+
 	private double number;
-	
+
 	public Divisor(double subject) {
 		this.number = subject;
 	}
-	
-	public void findDivisors(double number) {
+
+	public ArrayList<Double> findPrimeFactors(double n) {
 		
-		ArrayList<Integer> childPrimes = new ArrayList<Integer>();
-		
-		for(int i = 0; i < number; i++) {
-			
+		double number = n;
+		ArrayList<Double> result = new ArrayList<Double>();
+
+		while(number%2 == 0) {
+			result.add(2.0);
+			number = number / 2;
 		}
 		
+		for(int i = 3; i <= Math.sqrt(number); i+=2) {
+			while(number%i == 0) {
+				result.add((double) i);
+				number = number / i;
+			}
+		}
+		
+		if (number > 2) {
+			result.add(n);
+		}
+		
+		return result;
+
 	}
-	
-	//Returns true if given double is a prime (Negative, 0 and 1 will return true until fixed)
+
+	// Returns true if given double is a prime (Negative, 0 and 1 will return
+	// true until fixed)
 	public boolean isPrime(double number) {
-		
+
 		double squareRoot = Math.sqrt(number);
-		
-		for(int i = 2; i <= squareRoot; i++) {
-			if((number%i) == 0) {
+
+		for (int i = 2; i <= squareRoot; i++) {
+			if ((number % i) == 0) {
 				return false;
 			}
 		}
 		return true;
-		
+
 	}
-	
 
 }
